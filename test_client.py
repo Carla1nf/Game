bea58d6ad3389ddb3a74d5652db992fafbe485b0
matte <mattemusaa@gmail.com>
2023-06-11 00:28:36 -0300

    Llegue a la cima!!
    POV

--- test_client.py
+++ test_client.py
@@ -18,21 +18,56 @@ def find_direction(diccionario,direction,contador) -> float:
     if contador == 0 or contador == 1:
         return direction
     z = diccionario[contador]["z"]
-    beforez = diccionario[contador-1]["z"]
-    if z < beforez:
-        direction -= (math.pi/64)
+    before_z = diccionario[contador-1]["z"]
+    inclinacion_x = diccionario[contador]["inclinacion_x"]
+    before_inc = diccionario[contador-1]["inclinacion_x"]
+    inclinacion_y = diccionario[contador]["inclinacion_y"]
+    before_inc_y = diccionario[contador-1]["inclinacion_y"]
+    if z < before_z:
+        speed = 1
+        direction -= (math.pi/4)
+    elif z > 4999.9:
+        speed = 1
+    elif z > 4950:
+        speed = 25
+    else:
+        speed = 50
     print(direction)
     return direction
+
+def find_speed(diccionario,speed,contador):
+    contador -= 1
+    if contador == 0 or contador == 1:
+        return speed
+    z = diccionario[contador]["z"]
+    before_z = diccionario[contador-1]["z"]
+    inclinacion_x = diccionario[contador]["inclinacion_x"]
+    before_inc = diccionario[contador-1]["inclinacion_x"]
+    inclinacion_y = diccionario[contador]["inclinacion_y"]
+    before_inc_y = diccionario[contador-1]["inclinacion_y"]
+    if z < before_z:
+        speed = 1
+    elif z > 4999.9:
+        speed = 1
+    elif z > 4950:
+        speed = 25
+    else:
+        speed = 50
+    print(f"speed: {speed}")
+    return speed
 
 direction = ((5*math.pi)/4)
 contador = 0
 diccionario = {}
 player_info = c.get_data()[team2][climber1]
+speed = 50
 while not c.is_over():
     contador += 1
     direction = find_direction(diccionario,direction,contador)
-    c.next_iteration(team2,{climber1:{"direction":direction, 'speed':50}})
+    speed = find_speed(diccionario,speed,contador)
+    c.next_iteration(team2,{climber1:{"direction":direction, 'speed':speed}})
     player_info = c.get_data()[team2][climber1]
     diccionario[contador] = player_info
     print(diccionario[contador])
     time.sleep(0.1)
+print(c.get_data()[team2][climber1])
