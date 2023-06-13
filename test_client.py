12df9fead166bffd9fbf632e33e9c03779d0faba
matte <mattemusaa@gmail.com>
2023-06-12 22:41:16 -0300

    Graficos de a 4 que andan re piola
    PULeen esto

--- test_client.py
+++ test_client.py
@@ -1,16 +1,17 @@
 from communication.client.client import MountainClient
-from tests.main_animation_groups import main_graf
+from tests.main_animation_groups2 import main_graf
 import time
 import math
 
 class Climber:
-    def __init__(self,name,direction):
+    def __init__(self,name,direction,color):
+        self.color = color
         self.posible_cima = True
         self.name = name
         self.direction = direction
         self.contador = 0
         self.diccionario = {}
-        self.player_info = c.get_data()[team1][name]
+        self.player_info = ""
         self.speed = 50
         self.contador2 = 0
         
@@ -18,80 +19,80 @@ class Climber:
         if math.sqrt((x**2)+(y**2)) > 22900:
             return True
 
-    def cima_posible(self,diccionario,contador,posible_cima,contador2):
+    def cima_posible(self,contador,contador2):
         contador -= 1
         if contador == 0 or contador == 1:
-            return posible_cima
-        x = diccionario[contador]["x"]
-        y = diccionario[contador]["y"]
-        z = diccionario[contador]["z"]
-        before_z = diccionario[contador-1]["z"]
-        if self.peligro(x,y):
-            posible_cima=True
-            return posible_cima
-        if contador2 < 20 and posible_cima == False:
-            return posible_cima
+            return self.posible_cima
+        x = self.diccionario[contador]["x"]
+        y = self.diccionario[contador]["y"]
+        z = self.diccionario[contador]["z"]
+        before_z = self.diccionario[contador-1]["z"]
+        if math.sqrt((x**2)+(y**2)) > 22900:
+            self.posible_cima=True
+            return self.posible_cima
+        if contador2 < 20 and self.posible_cima == False:
+            return self.posible_cima
         if contador > 15:
-            loop_cords = (diccionario[contador-15]["x"],diccionario[contador-15]["y"],diccionario[contador-15]["z"])
-            actual_cords = (diccionario[contador]["x"],diccionario[contador]["y"],diccionario[contador]["z"])
+            loop_cords = (self.diccionario[contador-15]["x"],self.diccionario[contador-15]["y"],self.diccionario[contador-15]["z"])
+            actual_cords = (self.diccionario[contador]["x"],self.diccionario[contador]["y"],self.diccionario[contador]["z"])
             if (abs(loop_cords[0] - actual_cords[0]) < 200) and (abs(loop_cords[1] - actual_cords[1]) < 200) and (abs(loop_cords[2] - actual_cords[2]) < 2):
                 print("loop")
-                posible_cima = False
+                self.posible_cima = False
                 contador2 = 0
-                return posible_cima
-        if z > before_z and posible_cima == False:
+                return self.posible_cima
+        if z > before_z and self.posible_cima == False:
             print("cambio")
-            posible_cima = True
-        return posible_cima
+            self.posible_cima = True
+        return self.posible_cima
 
-    def find_direction(self,diccionario,direction,contador,posible_cima) -> float:
+    def find_direction(self,contador) -> float:
         """
         returns the next direction
         """
         contador -= 1
         if contador == 0 or contador == 1:
-            return direction
-        x = diccionario[contador]["x"]
-        y = diccionario[contador]["y"]
-        z = diccionario[contador]["z"]
-        before_x = diccionario[contador-1]["x"]
-        before_y = diccionario[contador-1]["y"]
-        before_z = diccionario[contador-1]["z"]
-        if self.peligro(x,y):
-            if self.peligro(before_x,before_y):
-                return direction
+            return self.direction
+        x = self.diccionario[contador]["x"]
+        y = self.diccionario[contador]["y"]
+        z = self.diccionario[contador]["z"]
+        before_x = self.diccionario[contador-1]["x"]
+        before_y = self.diccionario[contador-1]["y"]
+        before_z = self.diccionario[contador-1]["z"]
+        if math.sqrt((x**2)+(y**2)) > 22900:
+            if math.sqrt((before_x**2)+(before_y**2)) > 22900:
+                return self.direction
             print("PELIGRO!")
-            direction -= (7*math.pi/8)
-            return direction
-        if z < before_z and posible_cima == True:
-            direction -= (math.pi/4)
-        return direction
+            self.direction -= (7*math.pi/8)
+            return self.direction
+        if z < before_z and self.posible_cima == True:
+            self.direction -= (math.pi/4)
+        return self.direction
 
-    def find_speed(self,diccionario,speed,contador):
+    def find_speed(self,contador):
         contador -= 1
         if contador == 0 or contador == 1:
-            return speed
-        x = diccionario[contador]["x"]
-        y = diccionario[contador]["y"]
-        z = diccionario[contador]["z"]
-        before_z = diccionario[contador-1]["z"]
-        if self.peligro(x,y):
-            speed = 30
+            return self.speed
+        x = self.diccionario[contador]["x"]
+        y = self.diccionario[contador]["y"]
+        z = self.diccionario[contador]["z"]
+        before_z = self.diccionario[contador-1]["z"]
+        if math.sqrt((x**2)+(y**2)) > 22900:
+            self.speed = 30
         if z < before_z:
-            speed = 20
+            self.speed = 20
         else:
-            speed = 50
-        return speed
+            self.speed = 50
+        return self.speed
 
     def main_climb(self):
         self.contador += 1
         if self.posible_cima == False:
             self.contador2 += 1
-        self.posible_cima = self.cima_posible(self.diccionario,self.contador,self.posible_cima,self.contador2)
+        self.posible_cima = self.cima_posible(self.contador,self.contador2)
         if self.posible_cima == True:
             self.contador2 = 0 
-        self.direction = self.find_direction(self.diccionario,self.direction,self.contador,self.posible_cima)
-        self.speed = self.find_speed(self.diccionario,self.speed,self.contador)
+        self.direction = self.find_direction(self.contador)
+        self.speed = self.find_speed(self.contador)
         self.player_info = c.get_data()[team1][self.name]
         self.diccionario[self.contador] = self.player_info
         print(f"{self.name} {self.contador}: {self.diccionario[self.contador]}")
@@ -100,27 +101,27 @@ class Climber:
 
 
 team1 = "racing"
-climber1 = "leo messi"
-climber2 = "tete"
-climber3 = "lautaro martinez"
-climber4 = "copetti"
-equipo = [climber1,climber2,climber3,climber4]
-
+climber_1 = Climber("leo messi",((3*math.pi)/4),"royalblue")
+climber_2 = Climber("tete",((5*math.pi)/4),"mediumseagreen")
+climber_3 = Climber("lautaro martinez",((7*math.pi)/4),"navy")
+climber_4 = Climber("copetti",((math.pi)/4),"crimson")
+equipo = [climber_1,climber_2,climber_3,climber_4]
+nombres = [climber_1.name,climber_2.name,climber_3.name,climber_4.name]
+colores = {}
+for climber in equipo:
+    colores[climber.name] = climber.color
 
 
 
 c = MountainClient()
-c.add_team(team1,equipo)
+c.add_team(team1,nombres)
 
 c.finish_registration()
 
 print(c.get_data()[team1])
-climber_1 = Climber("leo messi",(math.pi))
-climber_2 = Climber("tete",((5*math.pi)/4))
-climber_3 = Climber("lautaro martinez",((3*math.pi)/4))
-climber_4 = Climber("copetti",((math.pi)/2))
+
 while not c.is_over():
-    c.next_iteration(team1,{climber1:climber_1.main_climb(),climber2:climber_2.main_climb(),climber3:climber_3.main_climb(),climber4:climber_4.main_climb()})
+    c.next_iteration(team1,{climber_1.name:climber_1.main_climb(),climber_2.name:climber_2.main_climb(),climber_3.name:climber_3.main_climb(),climber_4.name:climber_4.main_climb()})
     data = c.get_data()[team1]
-    main_graf(data)
+    main_graf(data,colores)
     time.sleep(0.1)
--- test_server.py
+++ test_server.py
@@ -1,8 +1,8 @@
 
 from communication.server.server import MountainServer
 
-from communication.server.mountain.easy_mountain import EasyMountain
+from communication.server.mountain.mishra_mountain import MishraBirdMountain
 
-m= EasyMountain(50,23000)
+m= MishraBirdMountain(50,23000)
 s= MountainServer(m, (14000,14000),50)
 s.start()
